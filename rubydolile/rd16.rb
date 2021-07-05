386f4f85bdfb6de355bd9a5897180d72ee2b58f2
endodaiki <82555115+0925Daiki@users.noreply.github.com>
2021-07-06 05:49:25 +0900

    7/6training

--- rubydolile/rd16.rb
+++ rubydolile/rd16.rb
@@ -1,7 +1,7 @@
 def count_code(str)
   #indexメソッドは、文字列や配列の中に指定した文字列が含まれていた場合、その文字列の開始位置を整数の値で返します。
   #str.index(検索したい文字列, [検索を開始する位置])
-  puts (str.index("code") + 1)
+  puts str.index("code", 0) + 1
 end
 
 count_code("aaacode")
